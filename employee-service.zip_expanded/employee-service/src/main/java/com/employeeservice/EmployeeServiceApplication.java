8b43e95ab3e7d4a68fc2accc66b460bbaca6349e
DeveloperOneDemo <92218513+DeveloperOneDemo@users.noreply.github.com>
2022-03-23 11:18:37 -0400

    resilence4j circuit breaker

--- employee-service.zip_expanded/employee-service/src/main/java/com/employeeservice/EmployeeServiceApplication.java
+++ employee-service.zip_expanded/employee-service/src/main/java/com/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class EmployeeServiceApplication {
 	
 	// i can configure my beans here beacuse this class can be used as a java based configuration file ( explore the @SpringBootApplication annotation on the top)
 	@Bean
+	@LoadBalanced // added because employee-service contacts department-service through api-gateway
 	public RestTemplate sendRestTemplate() {
 		return new RestTemplate();
 	}
--- employee-service.zip_expanded/employee-service/src/main/java/com/employeeservice/service/EmployeeService.java
+++ employee-service.zip_expanded/employee-service/src/main/java/com/employeeservice/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.employeeservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,12 +10,15 @@ import com.employeeservice.entity.Employee;
 import com.employeeservice.pojo.DepartmentPojo;
 import com.employeeservice.pojo.MergedEmployeeDepartmentPojo;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class EmployeeService {
 	
+	private static final String SERVICE_A = "serviceA";
+
 	@Autowired
 	EmployeeRepository employeeRepository;
 	
@@ -28,16 +32,29 @@ public class EmployeeService {
 		log.info("inside saveEmployee of EmployeeService");
 		return employeeRepository.save(employee);
 	}
-	
+	// create the fallback method fallBackGetEmployee() with the same signature of the method itself with an extra exception argument
+	@CircuitBreaker(name=SERVICE_A, fallbackMethod ="fallBackGetEmployee" )
 	// i want to work with RestTemplate here because i have to consume an endpoint of department-service
 	public MergedEmployeeDepartmentPojo getEmployee(int eid) {
 		log.info("inside getEmployee of EmployeeService");
 		Employee employee = employeeRepository.findById(eid).get();
 		// now that we have the employee info with the department id, we can consume the get a department endpoint from department-service
 		// for this we need RestTemplate
-		DepartmentPojo department = restTemplate.getForObject("http://localhost:5001/api/departments/"+employee.getDeptId(), DepartmentPojo.class);
+		// DepartmentPojo department = restTemplate.getForObject("http://localhost:5001/api/departments/"+employee.getDeptId(), DepartmentPojo.class);
+		DepartmentPojo department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/api/departments/"+employee.getDeptId(), DepartmentPojo.class);
 		
 		MergedEmployeeDepartmentPojo medp = new MergedEmployeeDepartmentPojo(employee, department);
 		return medp;
 	}
+	
+	// create the fallback method fallBackGetEmployee() with the same signature of the method itself with an extra exception argument
+	public MergedEmployeeDepartmentPojo fallBackGetEmployee(int eid, Exception exception) {
+		// here goes the code for the fall back if the circuit breakers goes to open state
+		// open state is reached when employee-service fails in consuming an endpoint of depart-service
+		System.out.println("Department-service failed");
+		return null;
+		
+		
+	}
+	
 }
